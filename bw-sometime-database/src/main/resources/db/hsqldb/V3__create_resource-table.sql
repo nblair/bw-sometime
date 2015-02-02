b0efc02237b73e3064ddc1579d87bc66c61394d9
Nicholas Blair <npblair@wisc.edu>
2015-02-01 20:11:57 -0600

    Change table spec regarding owner

--- bw-sometime-database/src/main/resources/db/hsqldb/V3__create_resource-table.sql
+++ bw-sometime-database/src/main/resources/db/hsqldb/V3__create_resource-table.sql
@@ -2,8 +2,7 @@ CREATE TABLE resources (
 	resource_id INTEGER NOT NULL, 
 	name VARCHAR(80) NOT NULL, 
 	description VARCHAR(255), 
-	owner_id INTEGER NOT NULL, 
+	owner_external_id VARCHAR(255) NOT NULL, 
 	mail VARCHAR(255), 
-	PRIMARY KEY (resource_id), 
-	CONSTRAINT resources_fk1 FOREIGN KEY (owner_id) REFERENCES OWNERS (internal_id)
+	PRIMARY KEY (resource_id)
 );
